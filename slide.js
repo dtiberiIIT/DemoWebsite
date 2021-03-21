a80869164f5aa0c43bc1cad2a089c6764d1b1d86
Dan Tiberi <dtiberi@hawk.iit.edu>
2021-03-21 18:20:23 -0500

    Implemented slide transition effect.

--- slide.js
+++ slide.js
@@ -1,14 +1,24 @@
 var slideIndex = 0;
+var lastSlide;
+
 window.onload = showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    }
+    var slides = document.getElementsByClassName("slide");
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex > slides.length) {slideIndex = 1;}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000); // Timer
+    slides[slideIndex-1].style.zIndex = "1";
+    setTimeout(showSlides, 3000); // Timer
+
+    var idxToHide = slideIndex-3;
+    if(idxToHide < 0){
+        idxToHide = slides.length + idxToHide;
+    }
+    slides[idxToHide].style.display = "none";
+    
+    if(slideIndex == 1){
+        console.log(slides.length)
+        slides[slides.length-1].style.zIndex = "0";
+    }
 }
